370c360c910cb3b28bfbd3ec2367b8d958fa4373
Breitner96 <jeanbreitnermora@gmail.com>
2021-07-26 10:38:34 -0500

    Correcion caracteres

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -76,7 +76,7 @@ class AuthController extends Controller
         $rules = [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6|confirmed'
+            'password' => 'required|string|confirmed'
         ];
 
         $messages = [
